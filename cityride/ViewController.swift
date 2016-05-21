1b15707ef0f2cfbd9ec1bcca1480b784b2fbbf79
Matthew Attou <matthewattou@gmail.com>
2016-05-21 00:07:17 -0700

    Initial Commit

---
+++ cityride/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cityride
+//
+//  Created by Matthew Attou on 5/21/16.
+//  Copyright © 2016 Matthew Attou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
